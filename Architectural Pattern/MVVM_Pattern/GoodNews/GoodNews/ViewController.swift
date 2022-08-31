e18ed51779550f746bed92e213765a494ecb05fb
ysh <happysh_s2@naver.com>
2022-09-01 03:51:12 +0900

    Create files

---
+++ Architectural Pattern/MVVM_Pattern/GoodNews/GoodNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by 양승현 on 2022/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
