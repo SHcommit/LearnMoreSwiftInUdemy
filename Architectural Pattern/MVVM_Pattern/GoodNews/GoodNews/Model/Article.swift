2411356a4ae45b75eb1bc8b6dd2b09c1a36e9e0c
ysh <happysh_s2@naver.com>
2022-09-01 17:17:59 +0900

    Implement Article

--- Architectural Pattern/MVVM_Pattern/GoodNews/GoodNews/Model/Article.swift
+++ Architectural Pattern/MVVM_Pattern/GoodNews/GoodNews/Model/Article.swift
@@ -5,4 +5,18 @@
 //  Created by 양승현 on 2022/09/01.
 //
 
-import Foundation
+/**
+ TODO : News API에 있는 데이터를 파싱했을 경우 원하는 뉴스 데이터를 저장하기 위한 Model
+ 
+ - Param <#ParamNames#> : <#Discription#>
+ - Param <#ParamNames#> : <#Discription#>
+ 
+ # Notes: #
+ 1. <#Notes if any#>
+ 
+ */
+
+struct Article: Decodable{
+    let title      : String
+    let description: String
+}
