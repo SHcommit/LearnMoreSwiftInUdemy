86fa6742ec8b798ce7b8014ca7297830d1d9ad92
ysh <happysh_s2@naver.com>
2022-09-01 20:42:48 +0900

    일단 내가 하던방식으로 MVC패턴으로 그냥 Alamfire로 news url파싱받아서 tableView를 구성한 상태이다.

--- Architectural Pattern/MVVM_Pattern/GoodNews/GoodNews/Controller/NewsListTableViewController.swift
+++ Architectural Pattern/MVVM_Pattern/GoodNews/GoodNews/Controller/NewsListTableViewController.swift
@@ -2,25 +2,29 @@ import UIKit
 
 class NewsListTableViewController : UITableViewController{
     var arr = [1,2,3,4,5,6,7,8,9,0,1,2,3,13,13,23,1,233,1,31,31,31,3,13,13,13,13,13,13,13,13,1,31,31,31,3,13,3,1]
+    var lists = [Article]()
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigation()
         setup()
-        
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arr.count
+        return lists.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell") ?? UITableViewCell(style: .default, reuseIdentifier: "newsCell")
-        cell.textLabel?.text = "\(arr[indexPath.row])"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "newsCell")
+        cell.textLabel?.text = "\(lists[indexPath.row].title)"
+        cell.detailTextLabel?.text = "\(lists[indexPath.row].description)"
         return cell
     }
     private func setup(){
-        let newsUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=dc7c80a136c9497c9a0f04d113b500e6"
-        Webservice().getArticles(url: newsUrl){ _ in
-            
+        let newsUrl = "https://newsapi.org/v2/top-headlines?country=kr&apiKey=dc7c80a136c9497c9a0f04d113b500e6"
+        Webservice().getArticles(url: newsUrl){ temp in
+            guard let list = temp else {return}
+            self.lists = list
+            self.tableView.reloadData()
         }
+        NSLog("\(lists.count)")
     }
 }
 
@@ -28,7 +32,7 @@ class NewsListTableViewController : UITableViewController{
 extension NewsListTableViewController{
     func setupNavigation(){
         //Navigation Title
-        self.navigationItem.title =  "GoodNews"
+        self.navigationItem.title =  "Daily 뉴스"
         
         //MARK: - appearance settings
         guard let navBar = self.navigationController?.navigationBar else{ return }
