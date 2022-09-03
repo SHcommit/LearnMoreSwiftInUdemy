0b4114bf6b6a9969dfc79cf5746554900e2f8095
ysh <happysh_s2@naver.com>
2022-09-03 17:29:32 +0900

    Implement initial UI

--- Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/Controllers/OrdersTableViewController.swift
+++ Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/Controllers/OrdersTableViewController.swift
@@ -5,4 +5,41 @@
 //  Created by 양승현 on 2022/09/02.
 //
 
-import Foundation
+import UIKit
+
+class OrdersTableViewController : UITableViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+        
+    }
+}
+
+
+extension OrdersTableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+}
+//MARK: - setupUI
+extension OrdersTableViewController {
+    //navigationBar
+    func setupNavigationBar(){
+        self.navigationItem.title = "Orders"
+        
+        let addBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(presentNewOrder(_:)))
+        self.navigationItem.rightBarButtonItem = addBtn
+    }
+    
+}
+
+//MARK: - Event Handler
+extension OrdersTableViewController {
+    @objc func presentNewOrder(_ sender: Any){
+        performSegue(withIdentifier: "addOrderSegue", sender: sender)
+    }
+}
