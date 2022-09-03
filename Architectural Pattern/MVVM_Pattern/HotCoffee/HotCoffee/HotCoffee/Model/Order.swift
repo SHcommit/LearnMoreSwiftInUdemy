44c2a240e2a501bf9bc88520466ca9452ae83501
ysh <happysh_s2@naver.com>
2022-09-03 19:40:44 +0900

    Add Order Model

--- Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/Model/Order.swift
+++ Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/Model/Order.swift
@@ -5,4 +5,22 @@
 //  Created by 양승현 on 2022/09/03.
 //
 
-import Foundation
+
+enum coffeeType: String, Codable{
+    case cappuccino
+    case lattee
+    case espressino
+    case americano
+    
+}
+enum coffeeSize: String, Codable{
+    case small
+    case medium
+    case large
+}
+struct Order: Codable {
+    let name : String
+    let email: String
+    let type : coffeeType
+    let size : coffeeSize
+}
