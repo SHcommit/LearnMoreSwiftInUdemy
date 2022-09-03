bb26a76a34d98f73a9bebcc20a6f8b8a4a174281
ysh <happysh_s2@naver.com>
2022-09-03 19:34:02 +0900

    Add load func

--- Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/Service/WebService.swift
+++ Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/Service/WebService.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Alamofire
+
+enum NetworkError: Error {
+    case decodingError
+    //URL 실제로 없을경우
+    case domainError
+    case urlError
+}
+struct Resource<T: Codable> {
+    let url: URL
+    
+}
+
+class Webservice {
+    func load<T>(resource: Resource<T>, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        let call = AF.request(resource.url, method: .post)
+        call.response{ res in
+            guard let jsonObj = try? res.result.get() , res.error == nil else {
+                completion(.failure(.domainError))
+                return
+            }
+            let res = try? JSONDecoder().decode(T.self, from: jsonObj)
+            if let _res = res {
+                //메인큐에서 비동기 처리하는 이유는 서버에서 받은 데이터는 바로 UI에 적용되기 때문. UI에서 뭘 하려면 메인큐 스레드에서 처리되는게 좋음.
+                DispatchQueue.main.async {
+                    completion(.success(_res))
+                }
+            }else {
+                completion(.failure(.decodingError))
+            }
+            
+            
+        }
+        
+    }
+}
