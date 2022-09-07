cede3cae81097e23f4eb2303da395d78cd61a4b6
ysh <happysh_s2@naver.com>
2022-09-07 16:55:49 +0900

    코딩키를 할때 API의 JSON키를 잘 봐야함.

--- Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/Model/Order.swift
+++ Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/Model/Order.swift
@@ -5,40 +5,10 @@
 //  Created by 양승현 on 2022/09/03.
 //
 
-
-//struct Order: Codable {
-//    let email: String
-//    let name : String
-//    let size : CoffeeSize
-//    let type : CoffeeType
-//
-//    init(from decoder: Decoder) throws {
-//        let values = try decoder.container(keyedBy: CodingKeys.self)
-//        email = try values.decodeIfPresent(String.self, forKey: .email) ?? ""
-//        name  = try values.decodeIfPresent(String.self, forKey: .name)  ?? "Guest"
-//        size  = try values.decode(CoffeeSize.self, forKey: .size)
-//        type  = try values.decode(CoffeeType.self, forKey: .type)
-//    }
-//}
-//
-//enum CoffeeType: String, Codable {
-//    case cappuccino
-//    case latte
-//    case espressino
-//    case cortado
-//
-//    init(from decoder: Decoder) throws {
-//        let label = try decoder.singleValueContainer().decode(String.self)
-//        let lowercaseLabel = label.lowercased()
-//        self = CoffeeType(rawValue: lowercaseLabel) ?? .latte
-//    }
-//}
-//
-//enum CoffeeSize: String, Codable {
-//    case small
-//    case medium
-//    case large
-//}
+/**
+ *decodeIfPresnt nil 반환 o
+ *decode nil 반환 x
+ */
 
 enum CoffeeType: String, Codable {
     case cappuccino
@@ -47,12 +17,12 @@ enum CoffeeType: String, Codable {
     case cortado
    
     init(from decoder: Decoder) throws {
+        //싱글 벨류 컨테이너는 single value의 데이터를 디코딩할때 사용한다. 커스텀 format에서 디코딩 데이터는 자주 사용된다.
         let label = try decoder.singleValueContainer().decode(String.self)
         let lowercaseLabel = label.lowercased()
         self = CoffeeType(rawValue: lowercaseLabel) ?? .latte
     }
 }
-
 enum CoffeeSize: String, Codable {
     case small
     case medium
@@ -65,6 +35,12 @@ struct Order: Codable {
     let size : CoffeeSize
     let type : CoffeeType
     
+    enum CodingKeys: String, CodingKey {
+        case name 
+        case email
+        case type
+        case size
+    }
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         email = try values.decodeIfPresent(String.self, forKey: .email) ?? ""
@@ -73,26 +49,3 @@ struct Order: Codable {
         type  = try values.decode(CoffeeType.self, forKey: .type)
     }
 }
-/*
-The problem with your code is case-sensitive properties.
-If the API returns these keys
-Name,
-Type,
-Email,
-Size,
-Type,
-then the Codable object should have the coding Keys to handle keys from API ```
-struct Order: Codable {
-    let email: String
-    let name : String
-    let size : CoffeeSize
-    let type : CoffeeType
-    enum CodingKeys: String, CodingKey{
-        case name = "Name"
-        case email = "Email"
-        case type = "Type"
-        case size = "Size"
-        
-    }
-} ```
-*/
