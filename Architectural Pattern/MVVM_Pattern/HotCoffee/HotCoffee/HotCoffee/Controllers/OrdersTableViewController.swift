13fdf1f1f7795f1cb4cabaa0d05256dd3c7aaa4a
ysh <happysh_s2@naver.com>
2022-09-07 18:59:53 +0900

    Add data in tableview cell

--- Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/Controllers/OrdersTableViewController.swift
+++ Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/Controllers/OrdersTableViewController.swift
@@ -8,24 +8,23 @@
 import UIKit
 import Alamofire
 class OrdersTableViewController : UITableViewController{
+    var orderListViewModel = OrderListViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
         populateOrders()
-        
-        
-        
     }
     
     //웹서비스에 요청
     private func populateOrders() {
         let coffeeOrdersURL = "https://warp-wiry-rugby.glitch.me/orders"
         let resource = Resource<[Order]>(url: coffeeOrdersURL)
-        Webservice().load(resource: resource) { result in
+        Webservice().load(resource: resource) { [weak self] result in
             
             switch result {
             case .success(let orders):
-                print(orders)
+                self?.orderListViewModel.ordersViewModel = orders.map{ OrderViewModel.init(order: $0)}
+                self?.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
@@ -35,12 +34,22 @@ class OrdersTableViewController : UITableViewController{
 
 
 extension OrdersTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return self.orderListViewModel.ordersViewModel.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        let vm = self.orderListViewModel.orderViewModel(at: indexPath.row)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderCell", for: indexPath)
+        
+        cell.textLabel?.text = vm.type
+        cell.detailTextLabel?.text = vm.size
+        
+        return cell
     }
 }
 //MARK: - setupUI
