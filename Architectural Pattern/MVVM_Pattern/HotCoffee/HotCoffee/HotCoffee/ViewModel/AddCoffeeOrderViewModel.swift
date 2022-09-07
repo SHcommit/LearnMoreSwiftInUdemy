6c6605f442cda73ef9ae478a83c60cc777c6caeb
ysh <happysh_s2@naver.com>
2022-09-07 18:57:13 +0900

    Create files

---
+++ Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/ViewModel/AddCoffeeOrderViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddCoffeeOrderViewModel.swift
+//  HotCoffee
+//
+//  Created by 양승현 on 2022/09/07.
+//
+
+import Foundation
---
+++ Architectural Pattern/MVVM_Pattern/HotCoffee/HotCoffee/HotCoffee/ViewModel/OrderViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OrderViewModel.swift
+//  HotCoffee
+//
+//  Created by 양승현 on 2022/09/07.
+//
+
+import Foundation
