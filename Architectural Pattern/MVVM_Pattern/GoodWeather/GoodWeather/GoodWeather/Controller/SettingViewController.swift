3ccf5987cd14b4916e5ecdebbb9ae39b7eac1f97
ysh <happysh_s2@naver.com>
2022-09-09 15:14:56 +0900

    Setup navBar background Appearance

--- Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeather/Controller/SettingViewController.swift
+++ Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeather/Controller/SettingViewController.swift
@@ -5,4 +5,22 @@
 //  Created by 양승현 on 2022/09/09.
 //
 
-import Foundation
+import UIKit
+
+class SettingViewController: UITableViewController {
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        self.navigationItem.title = "Settings"
+        setupNavigationBackground()
+    }
+    
+}
+
+extension SettingViewController {
+    func setupNavigationBackground() {
+        guard let navBar = self.navigationController?.navigationBar else {
+            return
+        }
+        self.setupNavigationAppearance(navBar: navBar)
+    }
+}
