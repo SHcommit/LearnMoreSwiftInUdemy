f22931f2f5b8399b63e66df2c8b831b79ad9255f
ysh <happysh_s2@naver.com>
2022-09-09 15:13:28 +0900

    Add setupNavigationAppearance(navBar:)

--- Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeather/Controller/Extensions.swift
+++ Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeather/Controller/Extensions.swift
@@ -5,4 +5,17 @@
 //  Created by 양승현 on 2022/09/09.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func setupNavigationAppearance(navBar: UINavigationBar) {
+        navBar.prefersLargeTitles = true
+        let appearance = UINavigationBarAppearance()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.titleTextAttributes = [.foregroundColor : UIColor.white]
+        appearance.backgroundColor = .orange
+        navBar.standardAppearance = appearance
+        navBar.scrollEdgeAppearance = appearance
+    }
+    
+}
