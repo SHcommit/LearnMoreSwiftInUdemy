7f782b70e5f681fd937ab6bd37f196b3fa786849
ysh <happysh_s2@naver.com>
2022-09-11 01:11:08 +0900

    Add saveCityBtn source code

--- Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeather/Controller/AddLocalWeather.swift
+++ Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeather/Controller/AddLocalWeather.swift
@@ -12,14 +12,19 @@ class AddLocalWeather : UIViewController {
     @IBOutlet weak var cityNameTextField: UITextField!
     
     @IBAction func saveCityButton(_ sender: Any) {
-        
+        if let city = cityNameTextField.text {
+            AddWeatherViewModel().addWeather(for: city) { vm in 
+                
+            }
+        }
     }
-    
+    private var addWeatherVM = AddWeatherViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Add City"
         setupNavigationBackground()
-        TestModule().start()
+        TestModule().testStartWebservice()
+        TestModule().testStartWeatherResponse()
     }
     override func viewWillAppear(_ animated: Bool) {
         setupNavigationBackground()
