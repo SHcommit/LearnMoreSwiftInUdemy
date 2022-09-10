556411e76df114fe0b4dd05e45fdd7fc2b007695
ysh <happysh_s2@naver.com>
2022-09-10 22:48:26 +0900

    Success uesr input to url string, parsing data from RESTful api

--- Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeather/Controller/AddLocalWeather.swift
+++ Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeather/Controller/AddLocalWeather.swift
@@ -7,11 +7,19 @@
 
 import UIKit
 
-class AddLocalWeather : UITableViewController {
+class AddLocalWeather : UIViewController {
+    
+    @IBOutlet weak var cityNameTextField: UITextField!
+    
+    @IBAction func saveCityButton(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.title = "AddWeathers"
+        self.navigationItem.title = "Add City"
         setupNavigationBackground()
+        TestModule().start()
     }
     override func viewWillAppear(_ animated: Bool) {
         setupNavigationBackground()
