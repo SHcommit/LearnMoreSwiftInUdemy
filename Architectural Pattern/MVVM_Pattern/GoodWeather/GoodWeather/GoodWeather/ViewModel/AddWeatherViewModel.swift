1dfd3c0700e4230b5139b6dd8a2702bce4e2790d
ysh <happysh_s2@naver.com>
2022-09-11 13:28:55 +0900

    Add addWeather(for:completion) by loading server's data

--- Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeather/ViewModel/AddWeatherViewModel.swift
+++ Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeather/ViewModel/AddWeatherViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+
+class AddWeatherViewModel {
+    
+    func addWeather(for city: String, completion: @escaping (WeatherViewModel) -> Void) {
+        let weatherResource = Resource<WeatherResponse>(searchedCity: city) { data in
+            return try? JSONDecoder().decode(WeatherResponse.self, from: data)
+        }
+        
+        Webservice().load(resource: weatherResource) { result in 
+            guard let weatherResource = result else {
+                return
+            }
+            let vm = WeatherViewModel(weather: weatherResource)
+            completion(vm)
+        }
+    }
+}
