5e06458d717b9a9a330c034bf37097232c6ec5e8
ysh <happysh_s2@naver.com>
2022-09-12 18:09:02 +0900

    Before testing toCelsius() and toFahrenheit(), 초기에 필요한 정보들을 구성하는 weatherListVM

--- Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeatherTests/WeatherListViewModelTests.swift
+++ Architectural Pattern/MVVM_Pattern/GoodWeather/GoodWeather/GoodWeatherTests/WeatherListViewModelTests.swift
@@ -6,30 +6,42 @@
 //
 
 import XCTest
+@testable import GoodWeather
 
+/*
+    1. addWeatherViewModel(_:) 는 단순히 list에 VM한개를 추가하는데 이를 테스트 할 수 있는데
+        비즈니스 관점에서 본다면  단순히 배열에 추가하는 것인데 이를 테스트하는 것은 나에게 가치 있는 테스트인가?
+        NO!!
+        단순히 배열 추가인데 단위 테스트 의미 없다.
+    > 테스트해야 할 것은 실제 로직이어야 한다.
+ 
+    In WeatherListViewModel.swift에서 가장 큰 로직은
+        - mutating private func toCelsius()
+        - mutating private func toFahrenheit()
+        함수이다. 이 두 로직은 의미 깊기에 테스트 할 가치가 있다.
+ 
+    TODO: 2. 온도가 섭씨인지 화씨인지, 성공적으로 convert할수 있는지 테스트 해보자.
+        테스트를 하기 위해선 몇가지 정보가 필요하다. list에 저장된 vm 한개라던지,, weather정보 etc
+ 
+ */
 class WeatherListViewModelTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private var weatherListVM: WeatherListViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.weatherListVM = WeatherListViewModel()
+        setupWeatherListVM()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func setupWeatherListVM() {
+        self.weatherListVM.addWeatherViewModel(WeatherViewModel(weather: WeatherResponse(local: "Seoul", sys: .init(country: "Korea"), main: .init(temp: 32, humidity: 0))))
+        
+        self.weatherListVM.addWeatherViewModel(WeatherViewModel(weather: WeatherResponse(local: "Daejeon", sys: .init(country: "Korea"), main: .init(temp: 72, humidity: 0))))
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    override func tearDown() {
+        <#code#>
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
