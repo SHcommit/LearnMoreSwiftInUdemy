8a652c25d7e41829b5cc56c3b940a18c5bce0bf8
ysh <happysh_s2@naver.com>
2022-09-12 16:12:16 +0900

    Delete mention

--- Architectural Pattern/MVVM_Pattern/UnitTest/CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
+++ Architectural Pattern/MVVM_Pattern/UnitTest/CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
@@ -26,20 +26,7 @@ class CalculatorAppTests: XCTestCase {
         let result = self.calculator.add(2,3)
         XCTAssertEqual(result, 5)
     }
-    
-    //MARK: - 기본 제공 함수들
-    
-    // test 가 실행된 후에 실행되는 함수
-    // 이 함수는 db에서 무언가를 작성하거나, 파일을 작성한 후에 테스트가 실행될 경우.
-    // 내가 실행한 테스트들을 삭제하는 로직을 tearDown()에서 할 수 있다.
-    // 테스트가 실제로 변경되지않도록!!! 삭제하는 로직을
-    
-    //만약 UserDefaults.default 나중에 사용할 수 있는 영구저장소를 test로 쓰고 있다면
-    // tearDown()에서 userDefaults값을 삭제할 수 있다.
     override func tearDown() {
         super.tearDown()
     }
-    
-    // 따라서 테스트는 한가지만 테스트하고 tearDown()을 통해 제거하는 원칙을 따른다.
-    // 신뢰할 수 없을때 하는게 test이다.
 }
