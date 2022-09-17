92fae324eb8d156e1e66f04c225c662b3297ec82
ysh <happysh_s2@naver.com>
2022-09-17 15:35:39 +0900

    Create SpotifyPlayer

---
+++ AutoLayout/SpotifyPalyer/SpotifyPlayer/SpotifyPlayer/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SpotifyPlayer
+//
+//  Created by 양승현 on 2022/09/17.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey:  Any]?) -> Bool {
+        
+        
+        return true
+    }
+}
+
