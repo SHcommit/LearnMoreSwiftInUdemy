f7853e02c7290512d9bceed7595531cb3fcd5d9b
ysh <happysh_s2@naver.com>
2022-09-17 22:09:02 +0900

    Ambiguity with nameLabel, textField. solution by rising nameLabel's Content Hugging .defaultLow's horizontal value. :)

--- AutoLayout/CHCRLab/CHCRLab/ViewController.swift
+++ AutoLayout/CHCRLab/CHCRLab/ViewController.swift
@@ -12,8 +12,69 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupViews()
     }
 
 
 }
 
+//MARK: - makeContents
+extension ViewController {
+    
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.backgroundColor = .yellow
+        
+        return label
+    }
+    
+    func makeTextField(withPlaceHolderText text: String) -> UITextField {
+        let textField  = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = text
+        textField.backgroundColor = .lightGray
+        
+        return textField
+    }
+}
+
+
+//MARK: - setupSubviews
+extension ViewController {
+    
+    func setupViews() {
+        let nameLabel = makeLabel(withText: "Name")
+        let textField = makeTextField(withPlaceHolderText: "Input user's name")
+        
+        setupNameLabelConstraint(label: nameLabel)
+        setupTextFieldConstraint(tf: textField, targetConstraint: nameLabel)
+    }
+    
+}
+
+// MARK: - 제약 추가해야쥥,..
+extension ViewController {
+    
+    func setupNameLabelConstraint(label: UILabel) {
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8)])
+        
+        label.setContentHuggingPriority(UILayoutPriority(rawValue: 251), for: .horizontal)
+    }
+    
+    func setupTextFieldConstraint(tf: UITextField, targetConstraint label: UILabel ) {
+        view.addSubview(tf)
+        
+        NSLayoutConstraint.activate([
+            tf.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: 8),
+            tf.firstBaselineAnchor.constraint(equalTo: label.firstBaselineAnchor),
+            tf.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)])
+        
+    }
+}
