72c28c4c0c57dd4f6de51838fb0902a5e787f270
ysh <happysh_s2@naver.com>
2022-09-30 01:35:56 +0900

    Create TabBar page  each Controllers

---
+++ Clone App/Instagram/Instagram/New Group1/Controller/ImageSelectorController.swift
@@ -0,0 +1,8 @@
+//
+//  ImageSelectedController.swift
+//  Instagram
+//
+//  Created by 양승현 on 2022/09/30.
+//
+
+import Foundation
---
+++ Clone App/Instagram/Instagram/New Group1/Controller/MainHomeController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  Instagram
+//
+//  Created by 양승현 on 2022/09/30.
+//
+
+import UIKit
+
+class MainTabController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
