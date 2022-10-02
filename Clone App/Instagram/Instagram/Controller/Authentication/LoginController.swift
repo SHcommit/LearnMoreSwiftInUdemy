1ddd82f2e316740047b2358c6302159fd2650a1c
ysh <happysh_s2@naver.com>
2022-10-02 23:27:36 +0900

    Create Authentication files

---
+++ Clone App/Instagram/Instagram/Controller/Authentication/LoginController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginController.swift
+//  Instagram
+//
+//  Created by 양승현 on 2022/10/02.
+//
+
+import Foundation
---
+++ Clone App/Instagram/Instagram/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationController.swift
+//  Instagram
+//
+//  Created by 양승현 on 2022/10/02.
+//
+
+import Foundation
