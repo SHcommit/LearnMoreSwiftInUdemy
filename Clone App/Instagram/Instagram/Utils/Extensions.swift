df39557e5817c913aab8a3ad2a5d36d49d6f1280
ysh <happysh_s2@naver.com>
2022-10-04 01:16:28 +0900

    Move setupViewGradientBackground() LoginCOntroller -> Extensions

--- Clone App/Instagram/Instagram/Utils/Extensions.swift
+++ Clone App/Instagram/Instagram/Utils/Extensions.swift
@@ -4,5 +4,19 @@
 //
 //  Created by 양승현 on 2022/10/04.
 //
+import UIKit
 
-import Foundation
+extension UIViewController {
+    func setupViewGradientBackground() {
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        
+        let gradientColors: [CGColor] = [
+                .init(red: 1.00, green: 0.37, blue: 0.43, alpha: 1.00),
+                .init(red: 1.00, green: 0.75, blue: 0.46, alpha: 1.00),
+                .init(red: 1.00, green: 0.76, blue: 0.44, alpha: 1.00)]
+        
+        gradient.colors = gradientColors
+        view.layer.addSublayer(gradient)
+    }
+}
