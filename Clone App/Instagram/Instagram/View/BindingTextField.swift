c4c8476faf48032b64d113e8a36993c6282b5e38
ysh <happysh_s2@naver.com>
2022-10-04 15:05:22 +0900

    바인딩 전용 textField 프로토콜 구

--- Clone App/Instagram/Instagram/View/BindingTextField.swift
+++ Clone App/Instagram/Instagram/View/BindingTextField.swift
@@ -5,4 +5,22 @@
 //  Created by 양승현 on 2022/10/04.
 //
 
-import Foundation
+import UIKit
+
+protocol BindingTextField where Self: UITextField{
+    
+    //MARK: - Properties
+
+
+//MARK: - Helpers
+
+    func bind(callback: @escaping (String)->Void)
+    
+    func addTargetTextField(textFieldDidChanged: Selector)
+
+
+//MARK: - Setup event handler
+
+    
+    func textFieldDidChanged(_ textField: UITextField)
+}
