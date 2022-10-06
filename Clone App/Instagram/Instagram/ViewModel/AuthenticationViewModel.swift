2eb7e16f558f4b115013f20017ff47f8dbe24402
ysh <happysh_s2@naver.com>
2022-10-06 23:42:16 +0900

    Add dynamic type for email,pw by binding UITextField's value change using bind(callback:) closure

--- Clone App/Instagram/Instagram/ViewModel/AuthenticationViewModel.swift
+++ Clone App/Instagram/Instagram/ViewModel/AuthenticationViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+
+struct LoginViewModel {
+    //MARK: - Properties
+    var email = Dynamic("")
+    var password = Dynamic("")
+    
+    var isValiedUserForm: Bool {
+        get {
+            return !(email.value.isEmpty) && !(password.value.isEmpty)
+        }
+    }
+}
+
+struct RegistrationViewModel {
+    
+}
+
+
+// T value가 변할 때마다 listener?(value) 클로저를 실행한다.
+// 이때 이 값을 TextField UI에 갱신할 것이다.
+class Dynamic<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    func bind(callback: @escaping Listener) {
+        listener = callback
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+}
