10b9bf60782a78bef015e5a9b18f609d78fb12bf
ysh <happysh_s2@naver.com>
2022-10-06 23:40:30 +0900

    Add BindingTextField protocol

--- Clone App/Instagram/Instagram/View/BindingTextField.swift
+++ Clone App/Instagram/Instagram/View/BindingTextField.swift
@@ -8,19 +8,12 @@
 import UIKit
 
 protocol BindingTextField where Self: UITextField{
-    
-    //MARK: - Properties
-
 
 //MARK: - Helpers
 
     func bind(callback: @escaping (String)->Void)
-    
     func addTargetTextField(textFieldDidChanged: Selector)
 
-
 //MARK: - Setup event handler
-
-    
     func textFieldDidChanged(_ textField: UITextField)
 }
