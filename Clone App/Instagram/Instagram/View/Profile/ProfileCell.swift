c4da92b588b406a2e170e92d8800fa930436bb34
ysh <happysh_s2@naver.com>
2022-10-27 20:47:15 +0900

    Initialize profile views

--- Clone App/Instagram/Instagram/View/Profile/ProfileCell.swift
+++ Clone App/Instagram/Instagram/View/Profile/ProfileCell.swift
@@ -5,4 +5,20 @@
 //  Created by 양승현 on 2022/10/27.
 //
 
-import Foundation
+import UIKit
+
+class ProfileCell: UICollectionViewCell {
+    
+    //MARK: - Properties
+    
+    //MARK: - LifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .lightGray
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) not implement")
+    }
+}
--- Clone App/Instagram/Instagram/View/Profile/ProfileHeader.swift
+++ Clone App/Instagram/Instagram/View/Profile/ProfileHeader.swift
@@ -5,4 +5,24 @@
 //  Created by 양승현 on 2022/10/27.
 //
 
-import Foundation
+import UIKit
+
+
+class ProfileHeader: UICollectionReusableView {
+    
+    //MARK: - Properties
+    
+    //MARK: - LifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemMint
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) not implement")
+    }
+}
+
+
+
