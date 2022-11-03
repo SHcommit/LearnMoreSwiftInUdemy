0f67e01d2b933dd0a9a8f2090d837af7b3b2a700
ysh <happysh_s2@naver.com>
2022-11-03 16:54:43 +0900

    Add defualt tableViewDataSource

--- Clone App/Instagram/Instagram/Controller/SearchController.swift
+++ Clone App/Instagram/Instagram/Controller/SearchController.swift
@@ -47,12 +47,11 @@ extension SearchController {
 extension SearchController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: REUSE_SEARCH_TABLE_CELL_IDENTIFIER, for: indexPath)
-        cell.backgroundColor = .systemPink
         return cell
     }
     
