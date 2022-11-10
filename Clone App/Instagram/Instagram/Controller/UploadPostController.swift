2ae8d4c279b305106ae898c42bc5d4adfe5d4fcf
ysh <happysh_s2@naver.com>
2022-11-10 21:06:35 +0900

    Implement default UI

--- Clone App/Instagram/Instagram/Controller/UploadPostController.swift
+++ Clone App/Instagram/Instagram/Controller/UploadPostController.swift
@@ -10,7 +10,8 @@ import UIKit
 class UploadPostController: UIViewController {
     
     //MARK: - Properties
-    
+    private let photoImageView: UIImageView = initPhotoImageView()
+    private let contentsTextView: UITextView = initContentsTextView()
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +23,16 @@ class UploadPostController: UIViewController {
 //MARK: - Helpers
 extension UploadPostController {
     func configureUI() {
+        view.backgroundColor = .white
         navigationItem.title =  "Upload Post"
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel,
                                                            target: self, action: #selector(didTapCancel))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share",
                                                             style: .done, target: self, action: #selector(didTapShare))
+        
+        setupSubviews()
+        setupSubviewsConstraints()
 
     }
 }
@@ -42,4 +47,60 @@ extension UploadPostController {
     @objc func didTapShare() {
         print("DEBUG: tap share event handing please ...")
     }
+}
+
+//MARK: - Setup subviews
+extension UploadPostController {
+    
+    func setupSubviews() {
+        view.addSubview(photoImageView)
+        view.addSubview(contentsTextView)
+    }
+    
+    func setupSubviewsConstraints() {
+        setupPhotoImageViewConstraints()
+        setupContentsTextViewConstraints()
+    }
+}
+
+//MARK: - Init properties
+extension UploadPostController {
+    
+    static func initPhotoImageView() -> UIImageView {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 10
+        iv.clipsToBounds = true
+        iv.image = UIImage(imageLiteralResourceName: "girl-1")
+        return iv
+    }
+    
+    static func initContentsTextView() -> UITextView {
+        let tv = UITextView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }
+}
+
+
+//MARK: - AutoLayout subview's constraint
+extension UploadPostController {
+    
+    func setupPhotoImageViewConstraints() {
+        NSLayoutConstraint.activate([
+            photoImageView.heightAnchor.constraint(equalToConstant: UPLOAD_POST_PHOTO_IMAGE_VIEW_SIZE),
+            photoImageView.widthAnchor.constraint(equalToConstant: UPLOAD_POST_PHOTO_IMAGE_VIEW_SIZE),
+            photoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            photoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
+    }
+    
+    func setupContentsTextViewConstraints() {
+        NSLayoutConstraint.activate([
+            contentsTextView.topAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: UPLOAD_POST_CONTENT_TOP_MARGIN),
+            contentsTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: UPLOAD_POST_CONTENT_SIDE_MARGIN),
+            contentsTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: UPLOAD_POST_CONTENT_SIDE_MARGIN),
+            contentsTextView.heightAnchor.constraint(equalToConstant: UPLOAD_POST_CONTENT_VIEW_SIZE)])
+    }
+    
 }
