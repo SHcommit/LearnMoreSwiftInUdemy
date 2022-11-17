b0101df46ed12f025d16ac94540be6e1e8861170
ysh <happysh_s2@naver.com>
2022-11-17 15:46:58 +0900

    aaa

---
+++ Combine/Section11/WeatherAPI/WeatherAPI/Utiles/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  WeatherAPI
+//
+//  Created by 양승현 on 2022/11/17.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    struct URLs {
+        
+        static let weather = "http://api.open"
+        
+    }
+}
---
+++ Combine/Section11/WeatherAPI/WeatherAPI/WebService/WebService.swift
@@ -0,0 +1,14 @@
+//
+//  WebService.swift
+//  WeatherAPI
+//
+//  Created by 양승현 on 2022/11/17.
+//
+
+import Foundation
+import Combine
+
+
+struct WebService {
+    func fetch
+}
