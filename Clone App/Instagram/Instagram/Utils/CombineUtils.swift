58d43d1d3ba95c0e1cc9072dfbc057f25f1136df
ysh <happysh_s2@naver.com>
2022-12-08 19:25:12 +0900

    UPdate sources

--- Clone App/Instagram/Instagram/Utils/CombineUtils.swift
+++ Clone App/Instagram/Instagram/Utils/CombineUtils.swift
@@ -5,4 +5,16 @@
 //  Created by 양승현 on 2022/12/08.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+class ConbineUtils {
+    
+    static func textfieldNotificationPublisher(withTF textField: UITextField) -> AnyPublisher<String,NotificationCenter.Publisher.Failure> {
+        return NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification,object: textField)
+            .map { return ($0.object as? UITextField)?.text ?? "" }
+            .eraseToAnyPublisher()
+    }
+
+}
