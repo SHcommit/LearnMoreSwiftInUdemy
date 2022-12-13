a5779432d3c577887ddc2b146030bad402c31c26
ysh <happysh_s2@naver.com>
2022-12-14 02:51:03 +0900

    초기 구현. 근데 get,set과 configureUserInfoUI state에서 업데이트 하는 부분 더 생각해봐야함.

--- Clone App/Instagram/Instagram/Protocols/ProfileHeaderViewModelProtocols.swift
+++ Clone App/Instagram/Instagram/Protocols/ProfileHeaderViewModelProtocols.swift
@@ -5,9 +5,56 @@
 //  Created by 양승현 on 2022/12/13.
 //
 
-import Foundation
-
+import UIKit
+import Combine
 
 protocol ProfileHeaderDelegate: class {
     func header(_ profileHeader: ProfileHeader)
+}
+
+
+protocol ProfileHeaderViewModelGetSetType {
+    
+    var user: UserInfoModel { get set }
+    
+    var profileImage: UIImage? { get set }
+    
+    var userStats: Userstats? { get set }
+}
+
+
+struct ProfileHeaderViewModelInput {
+    
+    let user: AnyPublisher<UserInfoModel,Never>
+    let profileImage: AnyPublisher<UIImage,Never>
+    let userStats: AnyPublisher<Userstats,Never>
+    
+}
+
+
+typealias ProfileHeaderViewModelOutput = AnyPublisher<ProfileHeaderState,ProfileHeaderErrorType>
+                                                        
+enum ProfileHeaderState {
+    case none,
+         configureUserInfoUI,
+         configureProfile(UIImage),
+         configureFollowUI((followers: Int,following: Int))
+         
+}
+
+enum ProfileHeaderErrorType: Error {
+    case fail
+
+    var errorDescription: String {
+        switch self {
+        case .fail:
+            return "none"
+        }
+    }
+
+}
+
+protocol ProfileHeaderViewModelType: ProfileHeaderViewModelGetSetType {
+    
+    func transform(input: ProfileHeaderViewModelInput)
 }
