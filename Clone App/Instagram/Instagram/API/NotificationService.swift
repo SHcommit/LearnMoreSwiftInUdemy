ac5a3a9f982ed30efe6a88b82eb66aa3eca81de4
ysh <happysh_s2@naver.com>
2023-01-10 23:42:45 +0900

    Add uploadNotification(toUser:type:post:)

--- Clone App/Instagram/Instagram/API/NotificationService.swift
+++ Clone App/Instagram/Instagram/API/NotificationService.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import Firebase
+
+struct NotificationService {
+    
+    static func uploadNotification(toUser uid: String, type: NotificationType, post: PostModel? = nil) {
+        guard let currentUid = Utils.pList.string(forKey: CURRENT_USER_UID) else { return }
+        var data = NotificationModel(uid: currentUid, timestamp: Timestamp(date: Date()), type: NotificationType(rawValue: type.rawValue) ?? .follow)
+        if let post = post {
+            data.postId = post.postId
+            data.postImageUrl = post.imageUrl
+        }
+        do {
+            _ = try COLLECTION_NOTIFICATION.document(uid).collection("user-notifications").addDocument(from: data)
+        }catch {
+            print("DEBUG: \(error.localizedDescription)")
+        }
+        
+    }
+    
+    static func fetchNotifications() {
+        
+    }
+}
